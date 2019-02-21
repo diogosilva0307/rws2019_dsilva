8660e669c53d98953b7cbc1e3f40f4ed4e770815
diogosilva0307 <diogo-silva3@hotmail.com>
2019-02-21 16:01:28 +0000

    Second try

--- player_dsilva/src/player_dsilva.cpp
+++ player_dsilva/src/player_dsilva.cpp
@@ -3,45 +3,42 @@
 #include <vector>
 
 using namespace std;
+using namespace boost;
+using namespace ros;
 
-namespace rws_dsilva
+namespace dsilva_ns
 {
 class Team
 {
 public:
-  Team(string team_name)
+  string team_name;
+  vector<string> player_names;
+  ros::NodeHandle n;
+
+  Team(string team_name_in)
   {
-    this->team_name = team_name;
-    nh.getParam("/team_" + team_name, player_names);
+    team_name = team_name_in;
+    // read the team players
+    n.getParam("/team_" + team_name, player_names);
   }
 
   void printInfo()
   {
     cout << "Team " << team_name << " has players: " << endl;
-    for (size_t i = 0; i < player_names.size(); i++)
-    {
-      cout << player_names[i] << endl;
-    }
+    for (size_t i = 0; i < player_names.size(); cout << player_names[i++] << endl)
+      ;
   }
 
   bool playerBelongsToTeam(string player_name)
   {
     for (size_t i = 0; i < player_names.size(); i++)
     {
-      if (player_name == player_names[i])
-      {
+      if (player_names[i] == player_name)
         return true;
-      }
-      else
-      {
-        return false;
-      }
     }
-  }
 
-  string team_name;
-  vector<string> player_names;
-  ros::NodeHandle nh;
+    return false;
+  }
 
 private:
 };
@@ -49,50 +46,43 @@ private:
 class Player
 {
 public:
-  Player(string name)  // Constructor
+  // properties
+  string player_name;
+
+  // methods
+  Player(string player_name_in)
   {
-    this->name = name;
-    this->team_name = team_name;
+    player_name = player_name_in;
   }
 
-  void setTeamName(string team_name)
+  void setTeamName(string team_name_in)
   {
-    if (team_name == "red" || team_name == "green" || team_name == "blue")
+    if (team_name_in == "red" || team_name_in == "green" || team_name_in == "blue")
     {
-      this->team_name = team_name;
+      team_name = team_name_in;
     }
     else
     {
-      cout << "Cannot set team name" << team_name << endl;
+      cout << "Cannot set team name " << team_name_in << endl;
     }
   }
 
   void setTeamName(int team_index)
   {
     if (team_index == 0)
-    {
       setTeamName("red");
-    }
     else if (team_index == 1)
-    {
       setTeamName("green");
-    }
     else if (team_index == 2)
-    {
       setTeamName("blue");
-    }
     else
-    {
       setTeamName("");
-    }
   }
 
-  string getTeamName(void)
+  string getTeamName()
   {
     return team_name;
-  }
-
-  string name;
+  };
 
 private:
   string team_name;
@@ -101,47 +91,6 @@ private:
 class MyPlayer : public Player
 {
 public:
-  MyPlayer(string name, string team_name) : Player(name)
-  {
-    team_red = (boost::shared_ptr<Team>)new Team("red");
-    team_green = (boost::shared_ptr<Team>)new Team("green");
-    team_blue = (boost::shared_ptr<Team>)new Team("blue");
-
-    if (team_red->playerBelongsToTeam(name))
-    {
-      team_mine = team_red;
-      team_hunters = team_blue;
-      team_preys = team_green;
-    }
-
-    else if (team_green->playerBelongsToTeam(name))
-    {
-      team_mine = team_green;
-      team_hunters = team_red;
-      team_preys = team_blue;
-    }
-
-    else if (team_blue->playerBelongsToTeam(name))
-    {
-      team_mine = team_blue;
-      team_hunters = team_green;
-      team_preys = team_red;
-    }
-    else
-    {
-      cout << "Something went wrong" << endl;
-      ROS_INFO_STREAM("Team: " << team_mine->team_name);
-    }
-    setTeamName(team_mine->team_name);
-  }
-
-  void printInfo()
-  {
-    ROS_INFO_STREAM("My name is " << name << " and my team is " << team_mine->team_name);
-
-    ROS_INFO_STREAM("I'm hunting team " << team_preys->team_name << " and fleeing from team "
-                                        << team_hunters->team_name);
-  }
   boost::shared_ptr<Team> team_red;
   boost::shared_ptr<Team> team_green;
   boost::shared_ptr<Team> team_blue;
@@ -149,37 +98,61 @@ public:
   boost::shared_ptr<Team> team_mine;
   boost::shared_ptr<Team> team_preys;
 
-private:
-};
-
-}  // namespace rws_dsilva
-
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "dsilva");
-
-  ros::NodeHandle nh;
-
-  rws_dsilva::MyPlayer player("dsilva", "red");
-  // player.setTeamName("green");
-  // player.setTeamName(1);
-  cout << "Hello world from " << player.name << " of team " << player.getTeamName() << endl;
-
-  // rws_dsilva::Team team_red("red");
-  // team_red.player_names.push_back("dsilva");
-  // team_red.player_names.push_back("moliveira");
-
-  while (ros::ok())
+  MyPlayer(string player_name_in, string team_name_in) : Player(player_name_in)
   {
-    /*team_red.printInfo();
-    if (team_red.playerBelongsToTeam("dsilva") == 1)
+    team_red = (boost::shared_ptr<Team>)new Team("red");
+    team_green = (boost::shared_ptr<Team>)new Team("green");
+    team_blue = (boost::shared_ptr<Team>)new Team("blue");
+
+    if (team_red->playerBelongsToTeam(player_name))
     {
-      cout << "This player belongs to the red team. " << endl;
+      team_mine = team_red;
+      team_preys = team_green;
+      team_hunters = team_blue;
+    }
+    else if (team_green->playerBelongsToTeam(player_name))
+    {
+      team_mine = team_green;
+      team_preys = team_blue;
+      team_hunters = team_red;
+    }
+    else if (team_blue->playerBelongsToTeam(player_name))
+    {
+      team_mine = team_blue;
+      team_preys = team_red;
+      team_hunters = team_green;
     }
     else
     {
-      cout << "This player doesn't belong to the red team. " << endl;
-    }*/
+      cout << "something wrong in team parametrizations!!!" << endl;
+    }
+
+    setTeamName(team_mine->team_name);
+
+    printInfo();
+  }
+
+  void printInfo(void)
+  {
+    ROS_INFO_STREAM("My name is " << player_name << " and my team is " << team_mine->team_name);
+    ROS_INFO_STREAM("I am hunting " << team_preys->team_name << " and fleeing from " << team_hunters->team_name);
+  }
+
+private:
+};
+
+}  // namespace dsilva_ns
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "dsilva");
+  ros::NodeHandle n;
+
+  dsilva_ns::MyPlayer player("dsilva", "green");
+  cout << "Hello World from " << player.player_name << " of team " << player.getTeamName() << endl;
+
+  while (ros::ok())
+  {
     ros::Duration(1).sleep();
     player.printInfo();
   }
